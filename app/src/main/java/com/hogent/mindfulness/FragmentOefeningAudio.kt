d1a8f3a8d63b527efb5c0106792bc9ab30efe2b8
NathanWesterlinck <nathan.westerlinck.y7716@student.hogent.be>
2018-10-21 13:02:39 +0200

    Audioscherm: layout gelukt, code behind getroubleshoot (niet gelukt, dus niet meegepusht) Documentatie toegevoegd

--- app/src/main/java/com/hogent/mindfulness/FragmentOefeningAudio.kt
+++ app/src/main/java/com/hogent/mindfulness/FragmentOefeningAudio.kt
@@ -1,23 +1,35 @@
 package com.hogent.mindfulness
 
-
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_fragment_oefeningtext.*
+import kotlinx.android.synthetic.main.fragment_fragment_oefeningaudio.*
 
+
+/**
+ * Deze klasse is een Fragment die het audioscherm van de oefeningdetails toont
+ * De layout die hiermee gelinkt is is fragment_fragment_oefeningaudio
+ */
 class FragmentOefeningAudio : Fragment() {
 
+    /**
+     * in de onCreateView-methode inflaten we onze layout fragment_fragment_oefeningaudio
+     */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_oefeningtext, container, false)
+        return inflater.inflate(R.layout.fragment_fragment_oefeningaudio, container, false)
     }
 
+    /**
+     * Deze methode wordt direct na de onCreateView-methode uitgevoerd, we zetten hier de tekst "Oefening - Audio' in de TextView fragment_oefeningaudio
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fragment_oefeningtext.text = "Oefening - Audio"
+        fragment_oefeningaudio.text = "Oefening - Audio"
     }
+
 }
--- app/src/main/java/com/hogent/mindfulness/FragmentOefeningInvoer.kt
+++ app/src/main/java/com/hogent/mindfulness/FragmentOefeningInvoer.kt
@@ -7,16 +7,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_fragment_oefeninginvoer.*
-import kotlinx.android.synthetic.main.fragment_fragment_oefeningtext.*
 
+/**
+ * Deze klasse is een Fragment die het invoerscherm van de oefeningdetails toont
+ * De layout die hiermee gelinkt is is fragment_fragment_oefeninginvoer
+ */
 class FragmentOefeningInvoer : Fragment() {
 
+    /**
+     * in de onCreateView-methode inflaten we onze layout fragment_fragment_oefeninginvoer
+     */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fragment_oefeninginvoer, container, false)
     }
 
+    /**
+     * Deze methode wordt direct na de onCreateView-methode uitgevoerd, we zetten hier de tekst "Oefening - Invoer' in de TextView fragment_oefeningbeschrijving
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         fragment_oefeningbeschrijving.text = "Oefening - Invoer"
