22708fde4e46542cd29bd1b438738387d14980f8
Michiel Leunens <michiel.leunens.y7743@student.hogent.be>
2018-12-05 21:57:20 +0100

    Animaties tussen fragments

--- app/src/main/java/com/hogent/mindfulness/MainActivity.kt
+++ app/src/main/java/com/hogent/mindfulness/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
@@ -264,6 +265,7 @@ class MainActivity : AppCompatActivity(), SessionFragment.SessionAdapter.Session
         when (item.itemId) {
             R.id.navigation_home -> {
                 supportFragmentManager.beginTransaction()
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                     .replace(R.id.session_container, sessionFragment)
                     .addToBackStack(null)
                     .commit()
@@ -271,6 +273,7 @@ class MainActivity : AppCompatActivity(), SessionFragment.SessionAdapter.Session
             }
             R.id.navigation_feedback -> {
                 supportFragmentManager.beginTransaction()
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                     .replace(R.id.session_container, postFragment)
                     .addToBackStack(null)
                     .commit()
@@ -278,6 +281,7 @@ class MainActivity : AppCompatActivity(), SessionFragment.SessionAdapter.Session
             }
             R.id.navigation_profile -> {
                 supportFragmentManager.beginTransaction()
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                     .replace(R.id.session_container, profileFragment)
                     .addToBackStack(null)
                     .commit()
